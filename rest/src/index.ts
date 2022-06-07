8fa9a044ef48c16600b33d597a9e2afb952d4b8a
kukjevov <kukjevov@gmail.com>
2022-06-07 11:39:49 +0200

    INT: updated export of middlewareTypes

--- rest/src/index.ts
+++ rest/src/index.ts
@@ -2,6 +2,6 @@ export * from './decorators';
 export * from './middlewares';
 
 export * from './misc/defaults';
-export type {middlewareTypes} from './misc/middlewareTypes';
+export * from './misc/middlewareTypes';
 export * from './misc/restHttpError.interface';
 export * from './misc/withRestClientContext';
